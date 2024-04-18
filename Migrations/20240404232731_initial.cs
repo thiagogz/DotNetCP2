3c26eac2a8169356595c9ed4b0f868b9374a2bb1
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-04-18 19:15:41 -0300

    Feat: ADD entityframework & migration

---
+++ Migrations/20240404232731_initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+#nullable disable
+
+namespace FIAP_MVC_RM97890.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "t_Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    UserName = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserEmail = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserPassword = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_Users");
+        }
+    }
+}
