3000dd7ee6909042d83392bbe871cc506d13083b
fariasao <enzofaarias2010@gmail.com>
2024-04-18 19:09:48 -0300

    feat: add onGet Login

---
+++ Views/Login/Login.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FIAP_MVC_PZ.Views.Login
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
